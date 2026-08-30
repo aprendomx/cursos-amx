dc7ee3c855c367dc796d8b9a4ad47830a81352dc
Julio Adrián <jadrian.s@gmail.com>
2026-08-30 16:53:09 -0600

    chore: eliminar useErrorReporter sin referencias (Sentry ya se cablea en main.js)

--- src/composables/useErrorReporter.js
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/vue'
-
-export function reportError(err, extra = {}) {
-  if (import.meta.env.VITE_SENTRY_DSN) {
-    Sentry.captureException(err, { extra })
-  } else {
-    console.error('[reportError]', err, extra)
-  }
-}
